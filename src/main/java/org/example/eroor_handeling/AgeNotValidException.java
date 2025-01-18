d2d97af92cb7d5210e545cbe795420122cfef466
n_bayat <N_bayat@ISC.co.ir>
2025-01-18 17:30:58 +0330

    one excption

---
+++ src/main/java/org/example/eroor_handeling/AgeNotValidException.java
@@ -0,0 +1,9 @@
+package org.example.eroor_handeling;
+
+public class  AgeNotValidException extends Exception  {
+
+    public AgeNotValidException(String message) {
+        super(message);
+    }
+
+}
